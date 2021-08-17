1faad3090be9c4267a3f9ab5804a015d560309e0
Ramaraj T <ramaraj.tt@gmail.com>
2021-08-17 13:57:26 +0530

    parse image and return pixels

---
+++ app.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+var pixels = require('image-pixels')
+var Promise = require('promise');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(fileUpload());
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+app.get("/", function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+app.post("/upload", function (req, res) {
+    console.log('upload started');
+    if (!req.files || Object.keys(req.files).length === 0) {
+        console.log('No files were uploaded.');
+        return res.status(400).send('No files were uploaded.');
+    }
+    let file = req.files.file;
+    console.log(file);
+    parseImage(file)
+        .then(result => {
+            var {data, width, height} = result;
+            console.log(width);
+            console.log(height);
+            return res.status(200).send(result);
+        })
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+async function parseImage(image) {
+    return new Promise(function (resolve, reject) {
+        var data = pixels(image)
+        resolve(data);
+    })
+}
